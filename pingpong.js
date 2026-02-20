013f5a1b9d0a4940ba089fcc63088af3062a6686
lipe2188269292 <filipe.passos.silva@escola.pr.gov.br>
2026-02-20 07:49:47 -0300

    Create pingpong.js

---
+++ pingpong.js
@@ -0,0 +1,29 @@
+// Criar Bolinha
+let raio = 7.5;
+let xbolinha = 100;
+let ybolinha = 200;
+let diametro = raio * 2;
+
+// Velocidade da bolinha 
+let velocidadeXBolinha = 6;
+let veloicdadeYBolinha = 6;
+
+// Minha Raquete
+let xRaquete = 5;
+let yRaquete = 150;
+
+// Raquete Oponente
+let xRaqueteOponente = 585;
+let yRaqueteOponente = 150;
+
+//Placar
+let meusPontos = 0;
+let pontosOponentes = 0;
+
+function setup() {
+   createCanvas(600,400);
+}
+
+function draw(){
+  
+}
